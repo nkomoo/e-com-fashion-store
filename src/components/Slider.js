b85628b2723988e49618ff574c5c36658a05a598
ChanteleNkomo <nkomochantele@gmail.com>
2022-12-13 14:56:32 +0000

    Changed slider image and contents

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -33,7 +33,7 @@ const Arrow = styled.div`
 `;
 
 const Wrapper = styled.div`
-height: 100%;
+height: 100vh;
 width: 100vw;
 display: flex;
 transition: all 1.5s ease;
@@ -51,44 +51,53 @@ background-color: #${(props) => props.bg};
 
 
 const ImageContainer = styled.div`
-flex: 1;
+/* flex: 1; */
 height: 100%;
-width: 100%;
+width: 1440px;
 `;
 
 const InfoContainer = styled.div`
-flex: 1;
-padding: 70px;
+/* flex: 1; */
+/* padding: 70px; */
+position: absolute;
+text-align: center;
 
-/*
-padding-left: 20px;
-padding-right: 70px; */
 `;
 
 const Title = styled.h1`
 font-size: 50px;
 text-align: center;
+color: white;
 `;
 
 const Desc = styled.p`
 margin: 30px 0px;
 font-size: 16px;
 font-weight: 500;
+text-align: center;
+color: white;
 `;
 
 
 const Button = styled.button`
 text-transform: uppercase;
-background-color: black;
-color: white;
+background-color: white;
+color: black;
 padding: 7px;
 font-size: 20px; 
 cursor: pointer; 
-border-radius: 15px;
+border: none;
+text-align: center;
+
+&:hover {
+opacity: .4;
+}
+
 `;
 
 const Image = styled.img`
-    height: 100%;
+    width: 100%;
+    margin-right: 150px;
 `;
 
 
